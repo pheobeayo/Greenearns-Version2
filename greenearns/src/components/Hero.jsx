80fe35aab699e9373e0523d890eae2caedf0f3c0
Ifeoluwa Sanni <pheobeayo@gmail.com>
2024-07-27 18:02:40 +0100

    update

--- greenearns/src/components/Hero.jsx
+++ greenearns/src/components/Hero.jsx
@@ -11,13 +11,13 @@ const Hero = () => {
         <div className="w-100 md:w-[100%] bg-white">
              <div className='w-100 md:w-[100%] lg:p-16 p-4  grid lg:grid-cols-2 h-[100vh] lg:gap-0 gap-4'>
                                         <div className="flex flex-col gap-4">
-                                                <h1 className='text-[30px] md:text-[34px] font-bold capitalize  text-[#0F160F]' >Empower climate change and sustainable carbon reduction through recycling.</h1>
-                                                <h3 className='text-[18px] md:text-[16px] text-[#0F160F]  '>Trade recyclables on the blockchain, earn rewards, and power the 
-                                                    <br/>green revolution with every sale and purchase</h3>
+                                                <h1 className='text-[40px] md:text-[40px]  font-serif font-bold capitalize  text-[#091913]'>Empower climate change and sustainable carbon reduction through recycling.</h1>
+                                                <h3 className='text-[20px] md:text-[20px]  font-serif text-[#091913]'>Trade recyclables on the blockchain, earn rewards, and power  
+                                                    <br/>the green revolution with every sale and purchase</h3>
                                                 <div className="flex item-centre gap-8 ">
-                                                        <Link to='/sell-product' style={{ textDecoration: 'none', color: 'white' }}>  <button className="bg-[#427142] hover:bg-[#DBE1D4] text-white font-bold py-2 px-4  rounded">
+                                                        <Link to='/sell-product' style={{ textDecoration: 'none', color: 'white' }}>  <button className="bg-[#427142] hover:bg-[#2B452B] text-white font-bold py-2 px-4  rounded">
                                                                 Sell Product    </button></Link>
-                                                        <Link to='/market-place' style={{ textDecoration: 'none', color: '#427142' }}>  <button className="bg-white hover:bg-[#427142] text-[#427142] font-bold py-2 px-4 border border-[#427142] rounded">
+                                                        <Link to='/market-place' style={{ textDecoration: 'none', color: '#427142' }}>  <button className="bg-white hover:bg-[#2B452B] text-[#427142] font-bold py-2 px-4 border border-[#427142] rounded">
                                                                 Buy Products    </button></Link>
                                                 </div>
 
